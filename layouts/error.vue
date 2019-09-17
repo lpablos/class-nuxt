a765d0eb87e656dd6eb3198afb3c33ec5807f7bb
lpablo <gevolutionxalapa>
2019-09-17 10:50:56 -0500

    paina de error

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>Uppsssss..... presentamos un error en la carga</h3>
+        <br>
+        <button @click="inicio">Cliceka para el inicio</button>
+    </div>
+</template>
+<script>
+export default {
+        
+    methods : {
+        inicio:function(){               
+            this.$router.push('/')
+        }
+    },
+}
+</script>
