55fb48c24efa320d4038301482b7d545b304f588
lpablo <gevolutionxalapa>
2019-09-17 10:12:35 -0500

    componente hijo

---
+++ pages/users.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <h1>Ususarios en la lista</h1>
+        <ul>
+            <nuxt-link to="users/1">First user</nuxt-link>
+            <nuxt-link to="users/2">Second user</nuxt-link>            
+        </ul>
+        <br>
+        <nuxt-child />        
+    </div>
+</template>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,10 +1,8 @@
-<<template>
-    <div class="container">
-        <h1>Ususarios en la lista</h1>
-        <ul>
-            <nuxt-link to="users/1">First user</nuxt-link>
-            <nuxt-link to="users/2">Second user</nuxt-link>            
-        </ul>
-        
+<template>
+    <div>
+        <br>
+        ------------------------------------------
+        <br>
+        <small>Desde el child componente</small>
     </div>
 </template>
